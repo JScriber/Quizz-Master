b95144cd6201ee244f2e08b88efecfa641bbf22a
Anthony <anthonygst26@gmail.com>
2019-05-23 18:37:10 +0200

    Close #4 Functional - Create question

--- app/tests/bdd/delete-question.spec.js
+++ app/tests/bdd/delete-question.spec.js
@@ -3,6 +3,9 @@ import { unit, given, when, then } from 'jest-bdd';
 import Question from '../../entities/question.js';
 import QuestionManager from '../../services/question-manager.js';
 
+// Creation du scope.
+const scope = {};
+
 /** L’utilisateur souhaite supprimer une question. */
 unit(
   given(`Étant donné que je suis sur la page de consultation des questions,
@@ -25,7 +28,7 @@ unit(
       then(`Rafraichir la page listant toutes les questions.
       La question supprimée n’est pas censée apparaître, ainsi nous ne devrions pas retrouver : “De quel côté agit le PHP ?”`, () => {
         
-        expect(true).toBe(scope.manager.getAll().find(e => e.title === scope.question.title) === undefined);
+        expect(scope.manager.getAll().find(e => e.title === scope.question.title)).toBeUndefined();
       })
     )
   )
